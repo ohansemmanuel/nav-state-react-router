4c3f28f8c03774bb40b823ad9097de28c6b97f87
Ohans Emmanuel <ohansemmanuel@gmail.com>
2018-07-15 18:35:54 +0100

    use the history.push method for navigating from ThumbsUp

--- src/containers/ThumbsUp.js
+++ src/containers/ThumbsUp.js
@@ -1,14 +1,14 @@
 import React from "react";
-import { Redirect } from "react-router-dom";
 import EmojiLand from "../components/EmojiLand";
 import thumbsUpImg from "../Images/thumbsup.png";
 import { DO_SOMETHING_OVER } from "../constants/action-types";
 import emojiLand from "./emojiLand";
 
-const ThumbsUp = ({ appState, handleEmojiAction, location }) => {
-  return appState === DO_SOMETHING_OVER ? (
-    <Redirect push to="/quiet" />
-  ) : (
+const ThumbsUp = ({ appState, handleEmojiAction, location, history }) => {
+  if (appState === DO_SOMETHING_OVER) {
+    history.push("/quiet");
+  }
+  return (
     <EmojiLand
       EmojiBg="linear-gradient(-225deg, #DFFFCD 0%, #90F9C4 48%, #39F3BB 100%)"
       EmojiImg={thumbsUpImg}
